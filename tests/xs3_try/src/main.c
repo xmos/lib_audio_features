4724010d1b6bd935ed65da67b04f693adbbde892
Ed <ed@xmos.com>
2021-01-18 14:13:06 +0000

    Building simple BFP FFT/iFFT

--- tests/xs3_try/src/main.c
+++ tests/xs3_try/src/main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+#include "xs3_math.h"
+
+#define N 512
+
 int main(void){
     printf("Hello\n");
+
+
+    // Initialize time domain data with samples.
+    int32_t buffer[N] = {0};
+    bfp_s32_t samples;
+    bfp_init_vect_s32(&samples, buffer, 0, N, 1);
+    // Perform the forward DFT
+    {
+        bfp_complex_s32_t* spectrum = bfp_fft_forward_mono(&samples);
+        // `samples` should no longer be used.
+        // Operate on frequency domain data using `spectrum`
+        
+
+        // Perform the inverse DFT to go back to time domain
+        bfp_fft_inverse_mono(spectrum); // returns (bfp_s32_t*) which is the address of `samples`
+    }
+    // Use `samples` again to use new time domain data. 
+
+    printf("Fin\n");
 }
