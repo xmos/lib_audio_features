f1240e446232e714a2aba5e924ad7527af15e116
Ed <ed@xmos.com>
2021-01-30 15:06:40 +0000

    test prog WIP

--- tests/xs3_try/src/main.c
+++ tests/xs3_try/src/main.c
@@ -13,7 +13,7 @@ unsigned time_start, time_stop;
 #define TIME_START(x) time_start = get_reference_time();
 #define TIME_STOP(msg) time_stop = get_reference_time(); printf("%s: %u ticks\n", msg, time_stop - time_start);
 
-#define BFP_FLOAT(mant, exp) ((float)mant * pow(2, exp))
+#define BFP_FLOAT(mant, exp) ((double)mant * pow(2, exp))
 
 
 void print_bfp(bfp_s32_t *bfp, unsigned idx){
@@ -36,8 +36,8 @@ void print_bfp_array_val(bfp_s32_t *bfp, unsigned n){
         int32_t* data = bfp->data;
         exponent_t exp = bfp->exp;
         int32_t mant = data[i];
-        // float val = (float)mant * pow(2, exp);
-        float val = BFP_FLOAT(mant, exp);
+        // float val = (double)mant * pow(2, exp);
+        double val = BFP_FLOAT(mant, exp);
         printf("%f\n", val);
     }
 }
@@ -67,34 +67,58 @@ void make_hanning(int32_t *window, unsigned size){
 
 void get_noise_array(int32_t *array, unsigned n);
 
-//We assume the input is both positive and normalised so that the mantissa as maximum precision
+//We assume the input is both positive and normalised so that the mantissa has maximum precision
 void bfp_ln_s32(bfp_s32_t *ln_out, bfp_s32_t *bfp_in, unsigned n){
     const unsigned ln_frac_bits = 24;
+    int apply_epsilon = 0; //For when input value approaches zero, we have to handle specially
+    int apply_clip = 0; //For when the input exponent permits a number greater than can be handled
 
-    exponent_t exp = bfp_in->exp;
+    exponent_t exp_in = bfp_in->exp;
     headroom_t hr = bfp_in->hr;
     int32_t* in_data_ptr = bfp_in->data;
     int32_t* ln_out_data_ptr = ln_out->data;
 
     //Using ln(m.2^exp) = ln(m) + exp.ln(2) we can calculate the ln value of q8_24 result = dsp_math_log(uq8_24 in);
-    printf("normalised exp: %d\n", exp);
+    // printf("normalised exp: %d\n", exp_in);
     //Next we need to denormalise the BFP values because the input and output from log is Q24.
-    exp += ln_frac_bits;
-    printf("denormalised exp: %d\n", exp);
+    exp_in += ln_frac_bits;
+    // printf("denormalised exp: %d\n", exp_in);
     
-    exponent_t exp_min = (exponent_t)((float)INT_MIN / log(2) / (1 << ln_frac_bits));
+    exponent_t exp_min = (exponent_t)((double)INT_MIN / log(2) / (1 << ln_frac_bits)); //-184
+    exponent_t exp_max = (exponent_t)((double)INT_MAX / log(2) / (1 << ln_frac_bits)) - 31; //154
+    int32_t clip_val = (int32_t)(log(2 * pow(2.0, exp_max)) * (1 << ln_frac_bits));//106.7446658062 << 24
+
     printf("exp_min: %d\n", (int)exp_min);
-    if(exp < exp_min){
-        printf("WARNING EXP MIN EXCEEDED - NEED TO HANDLE\n");
+    printf("exp_max: %d\n", (int)exp_max);
+    printf("clip_val: %d\n", (int)clip_val);
+
+    if(exp_in < exp_min){
+        printf("WARNING EXP MIN EXCEEDED\n");
+        apply_epsilon = 1;
+    }
+    if(exp_in > exp_max){
+        printf("WARNING EXP MAX EXCEEDED\n");
+        apply_clip = 1;
     }
 
-    const q8_24 log_adder = (q8_24)(((int64_t)exp * (int64_t)(log(2) * (1 << ln_frac_bits))));
-    printf("log_adder: %d\n", (int)log_adder);
+    const q8_24 log_adder = (q8_24)(((int64_t)exp_in * (int64_t)(log(2) * (1 << ln_frac_bits))));
+    // printf("log_adder: %d\n", (int)log_adder);
     for(int i=0; i<n; i++){
         uq8_24 uq8_24_in_data = in_data_ptr[i];
-        ln_out_data_ptr[i] = dsp_math_log(uq8_24_in_data) + log_adder;
-        printf("ln idx %u: %d\n", i, ln_out_data_ptr[i]);
+        if(apply_epsilon || uq8_24_in_data == 0){
+            ln_out_data_ptr[i] = INT_MIN;
+        }else if(apply_clip){
+            ln_out_data_ptr[i] = clip_val;
+
+        }else{
+            ln_out_data_ptr[i] = dsp_math_log(uq8_24_in_data) + log_adder;
+        // printf("ln idx %u mantissa: %d\n", i, (int)ln_out_data_ptr[i]);
+        }
     }
+    //We don't need to normalise since the output range of ln is -128 to +127 which covers
+    //2.57E-56 to 3.888E+55 with clipping protection, which should be enough and exceeds SPFP input range.
+    //log does a good job of squishfying the large dynamic range of the input
+    ln_out->exp = -ln_frac_bits;
 }
 
 int main(void){
@@ -109,7 +133,6 @@ int main(void){
     printf("max dsp_math_log: %d\n", (int)dsp_math_log(INT_MAX));
     //THIS FAILS
     //printf("max dsp_math_log: %d\n", (int)dsp_math_log(INT_MAX + 1));
-
 
     const unsigned calc_hr = 1; //Bool
 
@@ -190,15 +213,18 @@ int main(void){
     bfp_complex_s32_mag(&mag, spectrum_cplx);
     TIME_STOP("bfp_complex_s32_mag")
 
-    print_cpx_bfp_array(spectrum_cplx, 4);
-    print_bfp_array(&mag, 4);
+    print_cpx_bfp_array(spectrum_cplx, 10);
+    print_bfp_array(&mag, 10);
+
 
     // Use `samples` again to use new time domain data. 
 
     bfp_s32_t log_mag;
     bfp_s32_init(&log_mag, buffer_mulled, 0 /*exp*/, FFT_SIZE/2, calc_hr);
-    bfp_ln_s32(&log_mag, &mag, 4);
-    print_bfp_array(&log_mag, 4);
+    TIME_START()
+    bfp_ln_s32(&log_mag, &mag, FFT_SIZE/2);
+    TIME_STOP("bfp_ln_s32")
+    print_bfp_array(&log_mag, 10);
 
 
 
